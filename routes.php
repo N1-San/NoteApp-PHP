ddfc6ae4d0f528921e2209369c182800ce624e77
N1-San <33519431+N1-San@users.noreply.github.com>
2025-04-14 18:40:58 +0530

    refactor controllers

--- routes.php
+++ routes.php
@@ -2,35 +2,35 @@
 
 
 //main pages routes
-$router->get('/','controllers/index.php');
-$router->get('/about','controllers/about.php');
-$router->get('/contact','controllers/contact.php');
+$router->get('/','index.php');
+$router->get('/about','about.php');
+$router->get('/contact','contact.php');
 
 //notes listing routes
-$router->get('/note','controllers/notes/show.php');
-$router->get('/notes','controllers/notes/index.php')->only('auth');
+$router->get('/note','notes/show.php');
+$router->get('/notes','notes/index.php')->only('auth');
 
 //notes deletion routes
-$router->delete('/note-delete', 'controllers/notes/destroy.php');
+$router->delete('/note-delete', 'notes/destroy.php');
 
 
 //notes creation routes
-$router->get('/notes/create','controllers/notes/create.php');
-$router->post('/notes-create','controllers/notes/store.php');
+$router->get('/notes/create','notes/create.php');
+$router->post('/notes-create','notes/store.php');
 
 //notes update routes
-$router->get('/note/edit','controllers/notes/edit.php');
-$router->patch('/notes-update','controllers/notes/update.php');
+$router->get('/note/edit','notes/edit.php');
+$router->patch('/notes-update','notes/update.php');
 
 //registration routes
-$router->get('/register','/controllers/registration/create.php')->only('guest');
-$router->post('/register-create','/controllers/registration/store.php');
+$router->get('/register','registration/create.php')->only('guest');
+$router->post('/register-create','registration/store.php')->only('guest');
 
 //session routes
-$router->get('/login','/controllers/sessions/create.php')->only('guest');
-$router->post('/login-create','/controllers/sessions/store.php')->only('guest');
+$router->get('/login','sessions/create.php')->only('guest');
+$router->post('/login-create','sessions/store.php')->only('guest');
 
-//this route is not working rightn now
-$router->get('/logout','controllers/sessions/destroy.php');
 
-$router->get('/test','controllers/test.php');
+$router->get('/logout','sessions/destroy.php')->only('auth');
+
+$router->get('/test','test.php');
